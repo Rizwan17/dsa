80e10996ebb77f8b59d4fd27107d4d330b560fa1
Rizwan <rizwankhan.august16@gmail.com>
2022-07-02 16:42:36 +0530

    symmetric binary tree check

---
+++ tree/symetric-bt.js
@@ -0,0 +1,46 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+/**
+ * 
+ *          1
+ *      2       2
+ * 4       5 5      4
+ * 
+ * 
+ * */ 
+
+// sample tree
+const node = new Node(1);
+node.left = new Node(2);
+node.right = new Node(2);
+node.left.left = new Node(4);
+node.left.right = new Node(5);
+
+node.right.left = new Node(5);
+node.right.right = new Node(4);
+
+function isSymmetric(root){
+    return root === null || isSymmetricHelp(root.left, root.right);
+}
+
+function isSymmetricHelp(left, right){
+    if(left === null || right === null){
+        return left === right;
+    }
+
+    if(left.value !== right.value) return false;
+    return (
+        isSymmetricHelp(left.left, right.right)
+        &&
+        isSymmetricHelp(left.right, right.left)
+    );
+}
+
+console.log(isSymmetric(node));
