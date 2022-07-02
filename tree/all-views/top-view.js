68b9f5fc656b850e26c06630ea4e18133c8737b0
Rizwan <rizwankhan.august16@gmail.com>
2022-07-02 15:13:02 +0530

    top view of binary tree

---
+++ tree/all-views/top-view.js
@@ -0,0 +1,72 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// sample tree
+const node = new Node(1);
+node.left = new Node(2);
+node.right = new Node(3);
+node.left.left = new Node(4);
+node.left.right = new Node(5);
+
+node.right.left = new Node(6);
+node.right.right = new Node(7);
+
+/**
+ * 
+ * 
+ *          1
+ *      2       3
+ * 4       5 6      7
+ * 
+ * 
+ * 
+ * topView = [4, 2, 1, 3, 7]
+ * 
+ * vertical traversal = [[4], [2], [1, 5, 6], [3], [7]]
+ * 
+ * 
+ * */
+
+// using level order traversal
+
+function topView(node){
+    if(node === null) return;
+
+    const queue = [{node, pos: 0}];
+    const ans = {};
+    const topView = [];
+
+    while(queue.length){
+        
+        const item = queue.shift();
+        const x = item.pos;
+
+        if(x in ans){
+            ans[x].push(item.node.value);
+        }else{
+            ans[x] = [item.node.value];
+        }
+
+        if(item.node.left !== null) {
+            queue.push({ node: item.node.left, pos: x - 1 });
+        }
+        if(item.node.right !== null){
+            queue.push({ node: item.node.right, pos: x + 1 });
+        }
+    }
+
+    const keys = Object.keys(ans);
+    keys.sort((a, b) => a - b);
+    for(let x of keys){
+        topView.push(ans[x][0]);
+    }
+
+    return topView;
+}
+
+console.log(topView(node));
